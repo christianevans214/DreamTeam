a33611a5cc3806592d6c4001ba60b5b5b7b1e74c
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2015-07-28 11:41:59 -0400

    added review CRUD

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -5,6 +5,7 @@ module.exports = router;
 router.use('/members', require('./members'));
 router.use('/account', require('./account'));
 router.use('/albums', require('./albums'));
+router.use('/reviews', require('./reviews'));
 
 // Make sure this is after all of
 // the registered routes!
