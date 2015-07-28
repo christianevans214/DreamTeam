0a442f5893e0ca82f5e224f15d73da1ea7a10b76
Anna Goldberg <annalexgoldberg@gmail.com>
2015-07-28 11:28:37 -0400

    adds transaction and user routes

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,6 +4,7 @@ module.exports = router;
 
 router.use('/members', require('./members'));
 router.use('/account', require('./account'));
+router.use('/transaction', require('./transaction'));
 
 // Make sure this is after all of
 // the registered routes!
