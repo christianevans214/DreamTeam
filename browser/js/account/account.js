27d97ebf1f227fbe8049cb4a3b322a0fa91e1557
Christian Evans <christian.evans214@gmail.com>
2015-07-29 17:58:01 -0400

    merge

--- browser/js/account/account.js
+++ browser/js/account/account.js
@@ -1,31 +1,31 @@
-app.config(function ($stateProvider){
+app.config(function($stateProvider) {
 	$stateProvider.state('account', {
 		url: '/account/:id',
 		controller: 'AccountController',
 		templateUrl: 'js/account/account.html',
 		data: {
 			authenticate: true
-		}, 
+		},
 		resolve: {
-			user: function(UserFactory, $stateParams){
+			user: function(UserFactory, $stateParams) {
 				return UserFactory.getUser($stateParams.id);
 			}
 		}
 	})
 })
 
-app.factory('UserFactory', function($http){
+app.factory('UserFactory', function($http) {
 	return {
-		getUser: function(id){
+		getUser: function(id) {
 			return $http.get('api/account/' + id)
-			.then(function(res){
-				return res.data;
-			})
+				.then(function(res) {
+					return res.data;
+				})
 		}
 	}
 })
 
-app.controller('AccountController', function($scope, UserFactory, user){
+app.controller('AccountController', function($scope, UserFactory, user) {
 	$scope.user = user;
 	console.log("$scope.user", $scope.user)
 })
