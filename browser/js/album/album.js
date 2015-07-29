95389ecf00bb8e64b8e49c6035f4c95c24eecef0
Anna Goldberg <annalexgoldberg@gmail.com>
2015-07-28 20:27:55 -0400

    albumDetail in progress

---
+++ browser/js/album/album.js
@@ -0,0 +1,29 @@
+app.config(function($stateProvider){
+  $stateProvider.state('albumDetail', {
+    url: '/albums/:albumId',
+    controller: 'AlbumController',
+    templateUrl: 'js/album/album.html',
+    resolve: {
+      album: function(AlbumFactory, $stateParams){
+        return AlbumFactory.getAlbum($stateParams.albumId);
+      }
+    }
+  })
+})
+
+app.factory('AlbumFactory', function($http){
+  return{
+    getAlbum: function(id){
+      return $http.get('/api/albums/' + id)
+      .then(function(res){
+        return res.data;
+      })
+    }
+  }
+})
+
+app.controller('AlbumController', function($scope, album){
+  console.log('here');
+  $scope.album = album;
+  $scope.hello = "hello";
+})
--- browser/js/album/albumFactory.js
+++
@@ -1,3 +0,0 @@
-app.Factory('AlbumFactory', function(){
-  
-})
