833b515fb5a11399a7d0a6b310de4fc09cb9486a
Anna Goldberg <annalexgoldberg@gmail.com>
2015-07-30 17:28:59 -0400

    signup form and state and factory

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -8,9 +8,14 @@ app.config(function($stateProvider){
   })
 })
 
-app.controller('SignupCtrl', function($scope, Authservice, $state){
-  // $scope.signup = function(userData){
-  //   Auth.signup(userData)
-  //   .then()
-  // }
+app.controller('SignupCtrl', function($scope, AuthService, $state){
+  $scope.signup = function(userData){
+    AuthService.signup(userData)
+    .then(function(signedInUser){
+      $state.go('home') // {id: signedInUser._id}
+    })
+    .catch(function(){
+      $scope.error = 'Invalid signup credentials.';
+    })
+  }
 })
