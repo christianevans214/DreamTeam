f52bc25e1cc433aa340d98d7ff94d4af3364a964
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2015-07-31 11:09:38 -0400

    update routes for artists and transations

---
+++ server/app/routes/artists/index.js
@@ -0,0 +1,66 @@
+'use strict';
+var router = require('express').Router();
+var mongoose = require('mongoose');
+var _ = require('lodash');
+var User = mongoose.model('Artist');
+module.exports = router;
+
+//params for artist
+router.param('id', function(req, res, next, id){
+	Artist.findById(id).exec()
+	.then(function(artist){
+		if(artist) {
+			req.artist = artist;
+			next();
+		} else{
+			throw new Error('No artist found');
+		}
+	})
+	.then(null, next);
+})
+
+
+//GET All artists
+router.get('/', function(reArtistq, res, next) {
+	Artist.find({}).exec()
+		.then(function(artist) {
+			res.json(artist);
+		})
+		.then(null, next);
+})
+
+//GET One artist
+router.get('/:id', function(req, res){
+	res.json(req.artist);
+})
+
+
+//POST new artist
+router.post('/', function(req, res, next){
+	Artist.create(req.body)
+	.then(function(artist){
+		res.json(artist);
+	})
+	.then(null, next);
+})
+
+//PUT update artist
+router.put('/:id', function(req, res, next){  
+	_.extend(req.artist, req.body);  
+	req.artist.save()				
+	.then(function(artist){
+		res.json(artist);
+	})
+	.then(null, next);
+})
+
+//DELETE remove artist
+router.delete('/:id', function(req, res, next){
+	req.artist.remove()
+	.then(function(){
+		res.sendStatus(200);
+	})
+	.then(null, next);
+})
+
+
