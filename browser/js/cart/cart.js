a26c783b7207802d91fb6ad2401e03a646c295f1
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2015-08-02 14:16:28 -0400

    cart adds but must be reloaded

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -11,6 +11,31 @@ app.config(function($stateProvider){
   })
 })
 
+
+/*app.controller('CartController', function($scope, user, UserFactory, CartFactory, AlbumFactory){
+  //get the albums - this process seems very slow currently
+  $scope.user = user;
+
+  if(!$scope.albums) $scope.albums = [];
+  $scope.user.cart.forEach(function(item){
+    console.log("item", item);
+    AlbumFactory.getAlbum(item.album)
+    .then(function(album){
+      $scope.albums.push({album: album, quantity: item.quantity});
+      $scope.cartItems = $scope.albums;
+      return album;
+    })
+    .then(function(){
+      $scope.$apply();
+    })
+  })
+
+  
+  console.log("user cart", $scope.user.cart);
+
+  $scope.deleteFromCart = function(currentAlbum){
+    CartFactory.deleteAlbum(currentAlbum._id, $scope.user);*/
+
 app.controller('CartController', function($state, $scope, $rootScope, user, CheckoutFactory, UserFactory, CartFactory, AlbumFactory, localStorageService){
   //set user
   $scope.user = user;
@@ -18,14 +43,30 @@ app.controller('CartController', function($state, $scope, $rootScope, user, Chec
   //initialize albums array
   $scope.albums = [];
 
-  $rootScope.$on("editedCart", function(event, data) {
+/*  $rootScope.$on("editedCart", function(event, data) {
     console.log("event caught!", data, event);
     $scope.user = data;
     $scope.$apply();
+  })*/
+
+  $scope.user.cart.forEach(function(item){
+    console.log("item id", item.album)
+    AlbumFactory.getAlbum(item.album)
+    .then(function(album){
+      $scope.albums.push({album: album, quantity: item.quantity});
+      return album;
+    })
+    .then(function(){
+      $scope.cartItems = $scope.albums;
+      console.log("$scope.cartItems", $scope.cartItems)
+      $rootScope.$broadcast('loadedItems', $scope.cartItems);
+    })
   })
 
+
+
   //iterate through user cart and populate albums
-  $scope.getAlbumInfo = function(){
+/*  $scope.getAlbumInfo = function(){
     $scope.user.cart.forEach(function(item){
       AlbumFactory.getAlbum(item.album)
       .then(function(album){
@@ -38,7 +79,7 @@ app.controller('CartController', function($state, $scope, $rootScope, user, Chec
         $rootScope.$broadcast('loadedItems', $scope.cartItems)
       })
     })
-  }
+  }*/
 
 
   $rootScope.$on('loadedItems', function(event, data){
@@ -50,6 +91,7 @@ app.controller('CartController', function($state, $scope, $rootScope, user, Chec
   $scope.deleteFromCart = function(currentAlbum){
     CartFactory.deleteAlbum(currentAlbum, $scope.user.cart);
     UserFactory.updateUser($scope.user._id, $scope.user);
+    $rootScope.$apply();
   }
 
   //update album from user cart
