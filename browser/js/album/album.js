ae8efe983753b7554c3a3a3807545c17fe4e070d
Anna Goldberg <annalexgoldberg@gmail.com>
2015-08-02 18:48:32 -0400

    user cart works through local storage and persist user upon changes to local storage

--- browser/js/album/album.js
+++ browser/js/album/album.js
@@ -15,18 +15,48 @@ app.config(function($stateProvider){
   })
 })
 
-app.controller('AlbumController', function($scope, $rootScope, user, album, $state, AuthService, UserFactory, CartFactory, localStorageService, GuestCartFactory){
+app.controller('AlbumController', function($scope, $rootScope, user, album, $state, UserFactory, AuthService, CartFactory, localStorageService, GuestCartFactory){
 
   $scope.user = user;
   $scope.album = album;
 
 
+  // $scope.addToCart = function(currentAlbum){
+  //   if($scope.user){
+  //     CartFactory.addAlbum(currentAlbum, $scope.user);
+  //     UserFactory.updateUser($scope.user._id, $scope.user)
+  //     .then(function(updatedUser){
+  //       console.log('updated user', updatedUser);
+  //       $state.go('cart');
+  //     })
+  //   }else{
+  //     var guestCart = localStorageService.get('cart');
+  //     if(!guestCart){
+  //       guestCart = [{album: currentAlbum, quantity: 1}];
+  //       localStorageService.set('cart', guestCart);
+  //     }else{
+  //       GuestCartFactory.addAlbum(currentAlbum, guestCart);
+  //       console.log("guestCart", guestCart)
+  //       localStorageService.set('cart', guestCart);
+  //     }
+  //     $state.go('guestCart');
+  //   }
+  // };
+
+
   $scope.addToCart = function(currentAlbum){
     if($scope.user){
-      CartFactory.addAlbum(currentAlbum, $scope.user);
+      var userCart = localStorageService.get('userCart');
+      if(!userCart){
+        userCart = [{album: currentAlbum, quantity: 1}];
+        localStorageService.set('userCart', userCart);
+      }else{
+        CartFactory.addAlbum(currentAlbum, userCart);
+        localStorageService.set('userCart', userCart);
+      } 
+      $scope.user.cart = userCart;  
       UserFactory.updateUser($scope.user._id, $scope.user)
       .then(function(updatedUser){
-        console.log('updated user', updatedUser);
         $state.go('cart');
       })
     }else{
